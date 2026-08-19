1b6783b0d8824dacbaf99ca16d14bac36e176d49
Windows Space <200229812+WindowsSpace@users.noreply.github.com>
2026-08-19 17:10:35 +0300

    Обновление settings.py

--- settings.py
+++ settings.py
@@ -1,27 +1,36 @@
 import pygame
+from typing import Any
 
-WIDTH = 700
-HEIGHT = 700
-BACKGROUND_COLOR = (137, 145, 113)
-INACTIVE_COLOR = (129, 136, 111)
+# Разрешение игры/окна
+WIDTH:int = 774
+WIDTH_ALT:int = 1204
+HEIGHT:int = 946
 
-# Сетка
-BOARD_WIDTH = 10
-BOARD_HEIGHT = 16  
-CELL_SIZE = 43
-OFFSET_X = 5      
-OFFSET_Y = 5       
+# Задний фон
+BACKGROUND_COLOR:tuple[int, int, int] = (137, 145, 113)
+INACTIVE_COLOR:tuple[int, int, int] = (129, 136, 111)
 
-HUD_START_X = OFFSET_X + (BOARD_WIDTH * CELL_SIZE) + CELL_SIZE
-HUD_BOARD_Y = OFFSET_Y + (6 * CELL_SIZE)
+# Поле и размер клетки
+BOARD_WIDTH:int = 10
+BOARD_WIDTH_ALT:int = 20
+BOARD_HEIGHT:int = 20
+CELL_SIZE:int = 43
+
+OFFSET_X:int = 43
+OFFSET_Y:int = 43
+
+# Координаты мини-поля
+HUD_START_X:int = OFFSET_X + (BOARD_WIDTH * CELL_SIZE) + (2 * CELL_SIZE)
+HUD_BOARD_Y:int = OFFSET_Y + (7 * CELL_SIZE)
 
 # Направления
-DIR_UP = (0, -1)
-DIR_DOWN = (0, 1)
-DIR_LEFT = (-1, 0)
-DIR_RIGHT = (1, 0)
+DIR_UP:tuple[int, int] = (0, -1)
+DIR_DOWN:tuple[int, int] = (0, 1)
+DIR_LEFT:tuple[int, int] = (-1, 0)
+DIR_RIGHT:tuple[int, int] = (1, 0)
 
-DIR_TO_ANGLE = {
+# Направления
+DIR_TO_ANGLE:dict[Any, int] = {
     DIR_UP: 0,
     DIR_RIGHT: -90,
     DIR_DOWN: 180,
@@ -29,29 +38,37 @@ DIR_TO_ANGLE = {
 }
 
 # Управление
-PLAYER_MOVE_KEYS = {
+PLAYER_MOVE_KEYS:dict[int, Any] = {
     pygame.K_w: DIR_UP, pygame.K_UP: DIR_UP,
     pygame.K_s: DIR_DOWN, pygame.K_DOWN: DIR_DOWN,
     pygame.K_a: DIR_LEFT, pygame.K_LEFT: DIR_LEFT,
     pygame.K_d: DIR_RIGHT, pygame.K_RIGHT: DIR_RIGHT,
 }
-PLAYER_SHOOT_KEYS = {pygame.K_SPACE, pygame.K_RETURN}
+PLAYER_SHOOT_KEYS:set[int] = {pygame.K_SPACE, pygame.K_RETURN}
+PLAYER_MOVE_DELAY_MS:int = 150
 
-# Случайные спавны игрока
-PLAYER_SPAWN_POINTS = [
-    (5, 14), (2, 14), (8, 14),
-    (5, 11), (2, 11), (8, 11),
-    (5, 8),
+# Случайные спавны игрока (редактируемые с помощью режимов)
+PLAYER_SPAWN_POINTS:list[tuple[int, int]] = [
+    (5, 11)
 ]
 
-# Случайные спавны противника
-ENEMY_SPAWN_POINTS = [
-    (1, 1), (3, 1), (5, 1), (7, 1), (8, 1),
-    (1, 4), (8, 4),
-    (1, 7), (8, 7),
-    (3, 3), (6, 3), 
-    (4, 5), (6, 5),
-    (1, 14), (8, 14)
+# Случайные спавны противника (редактируемые с помощью режимов)
+ENEMY_SPAWN_POINTS:list[tuple[int, int]] = [
+    (1, 1),
+    (BOARD_WIDTH - 2, 1),
+    (1, BOARD_HEIGHT - 2),
+    (BOARD_WIDTH - 2, BOARD_HEIGHT - 2)
 ]
 
-PLAYER_MOVE_DELAY_MS = 150
+# Фигуры блоков
+BLOCK_SHAPES:list[list[tuple[int, int]]] = [
+    [(0, 0), (1, 0), (0, 1), (1, 1)],         # Квадрат 2x2
+    [(0, 0), (1, 0), (2, 0)],                 # Горизонтальная линия 1x3
+    [(0, 0), (0, 1), (0, 2)],                 # Вертикальная линия 3x1
+    [(0, 0), (1, 0), (2, 0), (1, 1)],         # Т-образная
+    [(0, 0), (0, 1), (0, 2), (1, 2)],         # L-образная
+    [(0, 0), (1, 0), (1, 1), (2, 1)],         # Z-образная
+    [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)], # Плюс
+    [(0, 0), (2, 0), (0, 1), (1, 1), (2, 1)], # U-образная арка
+    [(0, 0), (1, 1), (2, 2)]                  # Диагональ
+]
